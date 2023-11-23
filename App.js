727a723e17f9d3e51ec67f4769ff29e92da882bf
MA <ma@MAs-MacBook-Pro.local>
2023-11-23 13:12:55 +0300

    update

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
-import StartGameScreen from './screens/StartGameScreen';
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return (
-    <StartGameScreen/>
-  );
+  return <StartGameScreen />;
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
